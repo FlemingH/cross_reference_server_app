60df29b6b3093747a55b26609443715a16431d57
flm <lm.fei@outlook.com>
2021-06-20 21:39:23 +0800

    service show toast of received mock app message

--- app/src/main/java/com/volvo/crs/CRService.java
+++ app/src/main/java/com/volvo/crs/CRService.java
@@ -4,8 +4,10 @@ import android.app.Service;
 import android.content.Intent;
 import android.os.Binder;
 import android.os.IBinder;
+import android.os.Looper;
 import android.os.RemoteException;
 import android.util.Log;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 
@@ -17,6 +19,16 @@ public class CRService extends Service {
 
         @Override
         public int add(int arg1, int agr2) throws RemoteException {
+
+            new Thread(){
+                @Override
+                public void run() {
+                    Looper.prepare();
+                    Toast.makeText(getApplicationContext(), "VCC service received mock app message",
+                            Toast.LENGTH_SHORT).show();
+                    Looper.loop();
+                }
+            }.start();
 
             Log.d(TAG, "a1 = " + arg1 + " a2 = " + agr2);
             return arg1 + arg1;
